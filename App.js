e15d75f7e09ab8405f3b3d875d1cc268ebfffd56
Ismail <ismail.yimer.h@gmail.com>
2022-11-11 01:29:37 +0300

    fixed icons, added fonts and touchableOpacity

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, StyleSheet, Dimensions } from "react-native";
+import {
+    Text,
+    View,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import { Icon } from "react-native-elements";
 import { useState } from "react";
+import { useFonts } from "expo-font";
+import Logo from "./assets/pomo_doro.svg";
 
 export default function App() {
+    const [loaded] = useFonts({
+        "lora-bold": require("./assets/fonts/Lora-Bold.ttf"),
+        poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+    });
+
     const height = Dimensions.get("window").height;
     const [key, setKey] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
@@ -20,6 +33,8 @@ export default function App() {
         return `${minutes}:${seconds}`;
     };
 
+    if (!loaded) return null;
+
     return (
         <LinearGradient
             colors={["#FFF7EC", "#F2DCBF"]}
@@ -28,14 +43,19 @@ export default function App() {
                 height > 700 ? "py-20" : "py-12"
             } items-center justify-between gap-y-8`}
         >
-            <Text
-                style={styles.red}
-                className="text-5xl font-extrabold text-center"
-            >
-                POMO
-            </Text>
-            <View className="">
-                <Text className="text-3xl text-center py-2">
+            <View className="h-18 flex flex-row items-start justify-start">
+                <Text className="text-5xl" style={{ fontFamily: "lora-bold" }}>
+                    POMO-
+                </Text>
+                <View>
+                    <Logo width={40} height={40} />
+                </View>
+            </View>
+            <View>
+                <Text
+                    className="text-3xl text-center py-2"
+                    style={{ fontFamily: "poppins" }}
+                >
                     {duration === 25 * 60 ? "Focus" : "Break"}
                 </Text>
                 <View className="flex justify-center items-center">
@@ -49,7 +69,12 @@ export default function App() {
                     >
                         {({ remainingTime }) => (
                             <View className="bg-[#7E3C21] w-[230px] h-[230px] rounded-full items-center justify-center">
-                                <Text className="text-6xl text-white">
+                                <Text
+                                    accessibilityRole="timer"
+                                    accessibilityLiveRegion="assertive"
+                                    importantForAccessibility="yes"
+                                    className="text-6xl text-white"
+                                >
                                     {formatTime(remainingTime)}
                                 </Text>
                             </View>
@@ -58,67 +83,79 @@ export default function App() {
                 </View>
                 <View>
                     <View className="w-full flex-row justify-between">
-                        <View className="justify-center w-20 h-20 bg-[#7E3C21] mx-auto rounded-full">
-                            <Text
-                                onPress={() => {
-                                    setIsPlaying(false);
-                                    console.log("break", duration);
-                                    setDuration(5 * 60);
-                                    setIsPlaying(true);
-                                    setKey((prevKey) => prevKey + 1);
-                                }}
-                                className="text-white text-center"
-                            >
+                        <TouchableOpacity
+                            onPress={() => {
+                                setIsPlaying(false);
+                                setDuration(5 * 60);
+                                setIsPlaying(true);
+                                setKey((prevKey) => prevKey + 1);
+                            }}
+                            className="flex justify-center items-center w-20 h-20 bg-[#7E3C21] mx-auto rounded-full"
+                        >
+                            <Text className="text-white align-middle">
                                 Break
                             </Text>
-                        </View>
-                        <View className="justify-center w-20 h-20 bg-[#7E3C21] mx-auto rounded-full">
-                            <Text
-                                onPress={() => {
-                                    setIsPlaying(false);
-                                    console.log("focus", duration);
-                                    setDuration(25 * 60);
-                                    setIsPlaying(true);
-                                    setKey((prevKey) => prevKey + 1);
-                                }}
-                                className="text-white text-center"
-                            >
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setIsPlaying(false);
+                                setDuration(25 * 60);
+                                setIsPlaying(true);
+                                setKey((prevKey) => prevKey + 1);
+                            }}
+                            className="justify-center w-20 h-20 bg-[#7E3C21] mx-auto rounded-full"
+                        >
+                            <Text className="text-white text-center">
                                 Focus
                             </Text>
-                        </View>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
             <View className="flex-row items-center gap-2">
-                <View className="justify-center w-16 h-16 bg-[#7E3C21] mx-auto rounded-full">
+                <TouchableOpacity
+                    onPress={() => {
+                        setIsPlaying(false);
+                        setDuration(duration);
+                        setKey((prevKey) => prevKey + 1);
+                    }}
+                    className="justify-center w-16 h-16 bg-[#7E3C21] mx-auto rounded-full"
+                >
                     <Text className="text-white text-center">
-                        <FontAwesome5
-                            onPress={() => {
-                                setIsPlaying(false);
-                                console.log(duration);
-                                setDuration(duration);
-                                setKey((prevKey) => prevKey + 1);
-                            }}
-                            name={"stop"}
+                        <Icon
+                            name="stop"
+                            type="material-community"
+                            color="white"
+                            size={30}
                         />
                     </Text>
-                </View>
-                <View className="justify-center w-24 h-24 bg-[#7E3C21] mx-auto rounded-full">
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setIsPlaying(true)}
+                    className="justify-center w-24 h-24 bg-[#7E3C21] mx-auto rounded-full"
+                >
                     <Text className="text-white text-center">
-                        <FontAwesome5
-                            onPress={() => setIsPlaying(true)}
-                            name={"play"}
+                        <Icon
+                            name="play"
+                            type="material-community"
+                            color="white"
+                            size={50}
                         />
                     </Text>
-                </View>
-                <View className="justify-center w-16 h-16 bg-[#7E3C21] mx-auto rounded-full">
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setIsPlaying(false)}
+                    className="justify-center w-16 h-16 bg-[#7E3C21] mx-auto rounded-full"
+                >
                     <Text className="text-white text-center">
-                        <FontAwesome5
-                            onPress={() => setIsPlaying(false)}
-                            name={"pause"}
+                        <Icon
+                            name="pause"
+                            type="material-community"
+                            color="white"
+                            size={30}
                         />
                     </Text>
-                </View>
+                </TouchableOpacity>
             </View>
             <StatusBar style="auto" />
         </LinearGradient>
